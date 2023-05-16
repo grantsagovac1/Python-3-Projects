320ced04882bc56e446592fb3a5dce867ad7b387
grantsagovac1 <grantsagovac1@gmail.com>
2023-05-15 21:21:47 -0400

    added notes to explain parts of the code

--- quiz_game.py
+++ quiz_game.py
@@ -1,16 +1,27 @@
-print("Welcome to my computer quiz!")
+# Prints a message to the terminal
+print("Welcome to my computer quiz!") 
 
+# prompts an input asking the user if they want to play
 playing = input("Do you want to play? ")
 
+#  if the answer is not equal to yes it will quit
+# the .lower() method will let you type in all caps YES and still satisfy the == operator for "yes"
 if playing.lower() != "yes":
     quit()
 
+# After saying yes it prints a message
 print("Okay! Let's play :)")
+
+# creates an empty array score to store the score
 score = 0
 
+# prompts user with a question
 answer = input("What does CPU stand for? ")
+
+# same trick as before using the .lower() so the answer will be right whether or not they use capitals
 if answer.lower() == "central processing unit":
     print("Correct!")
+# adds 1 and sets a new value for the score array, then prints the score value as a string
     score += 1
     print(str(score))
 else:
